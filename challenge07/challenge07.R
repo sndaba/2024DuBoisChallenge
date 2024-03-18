f3af985dceb65f657b28557dbee21f19ccfacf1d
simisani ndaba <53818579+sndaba@users.noreply.github.com>
2024-03-18 15:14:44 +0200

    Add files via upload

---
+++ challenge07/challenge07.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggplot2)
+library(forcats)
+
+#extract and read data
+challenge07 <- read_csv("https://raw.githubusercontent.com/ajstarks/dubois-data-portraits/master/challenge/2024/challenge07/data.csv")
+view(challenge07)
+
+challenge07 <- rbind(c("Romaine",72.9363), challenge07)
+
+colnames(challenge07)[1] <- "Country" 
+colnames(challenge07)[2] <- "Proposion" 
+view(challenge07)
+
+challenge07$Proposion <- as.numeric(challenge07$Proposion) * 10
+
+ggplot(data = challenge07, aes(x  = Proposion, y = Country)) +  #Remove gaps in ggplot x-axis for missing data using factor()
+  geom_bar(stat="identity", fill = "green")  
+  #scale_x_reverse(breaks = seq(from = 0, to = 730, by = 1))
+
